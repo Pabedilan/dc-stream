6b47b4a43e21082d1ed94552ed380b56a907a877
DemasXI <109868586+DemasXI@users.noreply.github.com>
2022-07-30 23:25:11 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const express = require("express")
 const server = express()
 
 server.all("/", (req, res) => {
-  res.send('<meta http-equiv="refresh" content="0; URL=https://phantom.is-a.dev/support"/>')
+  res.send('<meta http-equiv="refresh" content="0; URL=https://loghkoo.demasxi.repl.co/"/>')
 })
 
 function keepAlive() {
   server.listen(3000, () => {
-    console.log("Server is ready.")
+    console.log("Server already")
   })
 }
 
